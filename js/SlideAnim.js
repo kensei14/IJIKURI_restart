b1333c57af031d907710b10107f78f05ba873c2f
kensei.yarimizu <kensei.yarimizu@10xlab.jp>
2012-11-06 18:33:25 +0900

    adjusted animation position

--- js/SlideAnim.js
+++ js/SlideAnim.js
@@ -5,14 +5,16 @@
 function setAnim() {
 	var default_left = parseInt($("#elem4").css("left").replace("px",""));
 	//var default_width = parseInt($("#elem4").css("width").replace("px",""));
-	var default_width = parseInt($("#elem4").css("width").replace("px","")) + parseInt($("#elem4").css("padding").replace("px","")) * 2;
+	var default_width = $("#elem4").width() + parseInt($("#elem4").css("padding").replace("px","")) * 2;
+	var default_height = $("#elem4").height() + parseInt($("#elem4").css("padding").replace("px","")) * 2;
 	
 	$('a[name=slide]').click(function(e) {
 		e.preventDefault();
 
 		var fading_elem = $(this).attr('href');
 
-		var target_left = parseInt($(fading_elem).css("left").replace("px","")) - default_width;
+		var target_left = parseInt($(fading_elem).css("left").replace("px","")) - default_width - 30;
+		var fading_elem_height = parseInt($(fading_elem).css("height").replace("px","")) + parseInt($(fading_elem).css("padding").replace("px","")) * 2;
 		$("#elem4").animate(
 			{ 'left':  target_left + 'px' },
 			{ 'duration': 400, 'easing': 'linear' }
@@ -20,7 +22,7 @@ function setAnim() {
 
 		var default_top = parseInt($("#elem4").css("top").replace("px",""));
 		$(fading_elem).animate(
-			{ 'top': default_top + 'px' },
+			{ 'top': default_top - (fading_elem_height - default_height)/2 + 'px' },
 			{ 'duration': 400, 'easing': 'linear' }
 		);
 	});
