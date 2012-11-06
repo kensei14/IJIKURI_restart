58a6b22aa96e8639ee86eae7c2bc4a60de8f9e80
kensei.yarimizu <kensei.yarimizu@10xlab.jp>
2012-11-06 23:44:54 +0900

    added a reflexive animation

--- js/SlideAnim.js
+++ js/SlideAnim.js
@@ -3,23 +3,16 @@
  */
 
 function setAnim() {
-	/*
-	var default_left = parseInt($("#elem4").css("left").replace("px",""));
-	var default_width = $("#elem4").width() + parseInt($("#elem4").css("padding").replace("px","")) * 2;
-	var default_height = $("#elem4").height() + parseInt($("#elem4").css("padding").replace("px","")) * 2;
-	*/
-		//var default_width = parseInt($("#elem4").css("width").replace("px",""));
-	
 	var target_top,
 		target_focus,
-		target_bottom = parseInt($("#elem100").css("top").replace("px",""));
-	//ml = Member_List ml_l=Member_LIist_Length
-	var ml = $("#member_list").children(),
+		target_bottom;
+	var ml = $("#member_list").children(), 	//ml = Member_List ml_l=Member_LIist_Length
 		ml_l = ml.length,
 		ml_targets = new Array(ml_l),
 		ml_width = $("#elem4").width() + parseInt($("#elem4").css("padding").replace("px","")) * 2,
 		ml_height = $("#elem4").height() + parseInt($("#elem4").css("padding").replace("px","")) * 2,
 		ml_top;
+	var pre_focus = 0;
 
 	$('a[name=slide]').click(function(e) {
 		e.preventDefault();
@@ -27,7 +20,7 @@ function setAnim() {
 		var focus_elem = $(this).attr('href'), //動かす対象の円コンテンツ
 			focus_elem_height; 
 		var find = false;
-		
+
 		ml_top  = parseInt($("#elem4").css("top").replace("px",""));
 		target_top = ml_top - pageHeight;
 		target_bottom = parseInt($("#elem100").css("top").replace("px",""));
@@ -42,51 +35,41 @@ function setAnim() {
 					{ 'left':  parseInt($(focus_elem).css("left").replace("px","")) - ml_width - 30 + 'px' },
 					{ 'duration': 400, 'easing': 'linear' }
 				);
-				$(focus_elem).animate(
+				ml_targets[i] = target_focus;
+/*				$(focus_elem).animate(
 					{ 'top': target_focus + 'px' },
 					{ 'duration': 300, 'easing': 'linear' }
 				);
+*/
 			} else if (find) {
-				//ml_targets[i] = target_bottom;
-				//alert($(ml[i]).attr("id") + "  " + target_top);
-				$(ml[i]).animate(
+				ml_targets[i] = target_bottom;
+/*				$(ml[i]).animate(
 					{ 'top': target_bottom + 'px' },
 					{ 'duration': 400, 'easing': 'linear' }
 				);
+*/
 			} else {
-				//ml_targets[i] = target_top;
-				//alert($(ml[i]).attr("id") + "  " + target_bottom);
-				$(ml[i]).animate(
+				ml_targets[i] = target_top;
+/*				$(ml[i]).animate(
 					{ 'top': target_top + 'px' },
 					{ 'duration': 400, 'easing': 'linear' }
 				);
-			}
-		}
-
-/*
-		var target_left = parseInt($(fading_elem).css("left").replace("px","")) - default_width - 30;
-		var fading_elem_height = parseInt($(fading_elem).css("height").replace("px","")) + parseInt($(fading_elem).css("padding").replace("px","")) * 2;
-		$("#elem4").animate(
-			{ 'left':  target_left + 'px' },
-			{ 'duration': 400, 'easing': 'linear' }
-		);
-
-		var default_top = parseInt($("#elem4").css("top").replace("px",""));
-		$(fading_elem).animate(
-			{ 'top': default_top - (fading_elem_height - default_height)/2 + 'px' },
-			{ 'duration': 400, 'easing': 'linear' }
-		);
 */
-
-		/*
-		var find = false;
-		for (var i; i < 4; i++) {
-			//とりあえず０を入れる。
-			top = 0;
-			//発見したら表示する。
-			if () { top = hoge; break; }
-			top = default;
+			}
+			nextAnim(0);	
 		}
-		*/
 	});
+	
+	function nextAnim(p) {
+		if (ml[p] == "undefined" | ml[p] == null) {
+			return false;
+		}
+		else {
+			$(ml[p]).animate(
+					{ 'top': ml_targets[p] + 'px' },
+					{ 'duration': 400, 'easing': 'linear' },
+					nextAnim(p+1)
+			);
+		}
+	}
 }
